3b144b6b0f8addd84e74482f6f9b7fb7ed1f8006
unknown <fer.romero1991@gmail.com>
2024-10-29 00:19:35 -0300

    servicios para reserva y estudio de baile

---
+++ src/main/java/com/itr/reserva_baile/service/ReservaService.java
@@ -0,0 +1,44 @@
+package com.itr.reserva_baile.service;
+
+import com.itr.reserva_baile.model.Reserva;
+import com.itr.reserva_baile.repository.ReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservaService {
+
+    private final ReservaRepository reservaRepository;
+
+    @Autowired
+    public ReservaService(ReservaRepository reservaRepository) {
+        this.reservaRepository = reservaRepository;
+    }
+
+    public Iterable<Reserva> getAllReservas() {
+        return reservaRepository.findAll();
+    }
+
+    public Reserva createReserva(Reserva reserva) {
+        return reservaRepository.save(reserva);
+    }
+
+    public Reserva updateReserva(Long id, Reserva reservaDetails) {
+        return reservaRepository.findById(id)
+                .map(reserva -> {
+                    reserva.setFecha(reservaDetails.getFecha());
+                    reserva.setHora(reservaDetails.getHora());
+                    reserva.setDuracion(reservaDetails.getDuracion());
+                    reserva.setEstado(reservaDetails.getEstado());
+                    reserva.setUsuarioId(reservaDetails.getUsuarioId());
+                    reserva.setClaseId(reservaDetails.getClaseId());
+                    reserva.setEstudioId(reservaDetails.getEstudioId());
+                    return reservaRepository.save(reserva);
+                })
+                .orElseThrow(() -> new RuntimeException("Reserva no encontrada"));
+    }
+
+    public void deleteReserva(Long id) {
+        reservaRepository.deleteById(id);
+    }
+}
