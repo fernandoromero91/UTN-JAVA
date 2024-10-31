034eb72ae6faa5e757c534a7a6c5857c46b9f460
unknown <fer.romero1991@gmail.com>
2024-10-31 00:56:11 -0300

    le agrego validaciones de campos obligatorios a reserva

--- src/main/java/com/itr/reserva_baile/model/Reserva.java
+++ src/main/java/com/itr/reserva_baile/model/Reserva.java
@@ -7,6 +7,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,17 +18,27 @@ public class Reserva {
     @Id
     private Long id;
 
+    @NotNull(message = "El usuarioId es obligatorio")
     @Column("usuario_id")
     private Long usuarioId;
 
+    @NotNull(message = "La claseId es obligatoria")
     @Column("clase_id")
     private Long claseId;
 
+    @NotNull(message = "El estudioId es obligatorio")
     @Column("estudio_id")
     private Long estudioId;
 
+    @NotEmpty(message = "La fecha es obligatoria")
     private String fecha; // fecha de la reserva
+
+    @NotEmpty(message = "La hora es obligatoria")
     private String hora;  // hora de la reserva
-    private int duracion; // duración de la reserva en minutos
+
+    @NotNull(message = "La duración es obligatoria")
+    private Integer duracion; // duración de la reserva en minutos
+
+    @NotEmpty(message = "El estado es obligatorio")
     private String estado; // Ej. "confirmada", "cancelada", "modificada"
 }
